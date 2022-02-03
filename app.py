9bb4719272145b88cc8a1fe5e8f979a4c0ac20b1
luisgar289 <1720110205@utectulancingo.edu.mx>
2022-02-03 19:06:05 +0000

    Trabajo clase

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 import web
 import firebase_config as token
 import pyrebase
+import json
+
 #crea las urls para la aplicación web
 urls = (
     '/', 'index',
@@ -19,18 +21,25 @@ class index: #pagina raíz
 
 class login: #pagina login
     def GET(self):
-        return render.login()
+        message = None
+        return render.login(message)
 
     def POST(self):
-        #obtiene los datos del formulario
-        formulario = web.input() 
-        email = formulario.email 
-        password= formulario.password
-        user = auth.sign_in_with_email_and_password(email, password) #autentica los datos con firebase
-        if user is not None: #evalua la respuesta de firebase
-            return "Bienvenido Usuario"
-        else:
-            None
+        try:
+            #obtiene los datos del formulario
+            message = None
+            formulario = web.input() 
+            email = formulario.email 
+            password= formulario.password
+            user = auth.sign_in_with_email_and_password(email, password) #autentica los datos con firebase
+            message = "Ok"
+            return render.login(message)
+        except Exception as error:
+            formato = json.loads(error.args[1])
+            error = formato['error']
+            message = error['message']
+            print("Error Login.POST: {}".format(message))
+            return render.login(message)
 
 class signup: #pagin sign up
     def GET(self):
@@ -42,8 +51,9 @@ class signup: #pagin sign up
         email = formulario.email
         password= formulario.password
         auth.create_user_with_email_and_password(email, password) #crea un nuevo usuario en firebase
-        return "Usuario Creado"
+        return render.login()
 
 if __name__ == "__main__":
+    web.config.debug = False
     app.run()
     
