77ae8452d2a23bdecda05d5b04a72e6ef6772963
luisgar289 <1720110205@utectulancingo.edu.mx>
2022-02-11 04:16:45 +0000

    Casi terminado

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ import json
 urls = (
     '/', 'index',
     '/login', 'login',
-    '/signup', 'signup'
+    '/signup', 'signup',
+    '/bienvenida','bienvenida',
+    '/logout', 'logout'
 )
 app = web.application(urls, globals())
 render = web.template.render('templates') #directorio de los archivos html
@@ -30,12 +32,18 @@ class login: #pagina login
             email = formulario.email 
             password= formulario.password
             user = auth.sign_in_with_email_and_password(email, password) #autentica los datos con firebase
-            return render.bienvenida(email) #muestra la pagina de bienvenida.html, pero no redirecciona
+            informacion = auth.get_account_info(user['idToken'])
+            usuarios = informacion['users']
+            usuario = usuarios[0]
+            localId = usuario['localId']
+            web.setcookie('localid', localId)
+            return web.seeother('bienvenida') #muestra la pagina de bienvenida.html, pero no redirecciona
         except Exception as error: #recopila y muestra los datos de algun error
             formato = json.loads(error.args[1])
             error = formato['error']
             message = error['message']
-            print("Error Login.POST: {}".format(message)) #imprime en la terminal el error
+            print(formato)
+            #print("Error Login.POST: {}".format(message)) #imprime en la terminal el error
             if message == "INVALID_EMAIL": 
                 return render.loginerror_mail() #muestra la pagina loginerror_mail
             elif message == "EMAIL_NOT_FOUND": 
@@ -64,6 +72,25 @@ class signup: #pagin sign up
                 return render.registrarerror_mail() #muestra la pagina registrarerror_mail
             else:
                 return render.registrarerror_pass() #muestra la pagina registrarerror_pass
+
+class bienvenida: 
+    def GET(self):
+        return render.bienvenida()
+
+    def POST(self):
+        cookie = web.cookies().get('localid')
+        if cookie is not None:
+            return web.seeother('bienvenida')
+        else:
+            return web.seeother('login')
+    
+class logout:
+    def GET(self):
+        web.setcookie('localid', None)
+        cookie = web.cookies().get('localid')
+        print(cookie)
+        return web.seeother('login')
+            
 
 if __name__ == "__main__":
     web.config.debug = False
