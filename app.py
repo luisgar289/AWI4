eb07f782cfc5769ff383f284535f9b5412004470
luisgar289 <1720110205@utectulancingo.edu.mx>
2022-02-20 11:02:25 -0600

    Hey

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ class recuperar:
             print("Error Login.POST: {}".format(message))
             return render.recuperar(message)
     
-if __name__ == "__main__": #ejecuta la aplicacion web
+if __name__ == "__main__": 
     web.config.debug = False
     app.run()
     
