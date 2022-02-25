bef862dbc82a4c0546c568501ad2f61098d4a02e
luisgar289 <1720110205@utectulancingo.edu.mx>
2022-02-25 11:59:39 -0600

    Actividad finalizada

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 
 #url y funcion de la aplicacion web
 urls = (
-    '/', 'index',
+    '/', 'bienvenida',
     '/login', 'login',
     '/signup', 'signup',
     '/bienvenida','bienvenida',
@@ -17,7 +17,8 @@ urls = (
     '/actualizar_usuario/(.*)', 'actualizar_usuario',
 )
 app = web.application(urls, globals())
-render = web.template.render('templates') #directorio de los archivos html
+render = web.template.render('templates/', base = 'layout') #platillas html con una base
+render_plain = web.template.render('templates/') #platillas html sin una base
 firebase = pyrebase.initialize_app(token.firebaseConfig) #conexión a firebase
 auth = firebase.auth() #verifica la conexion a firebase
 db = firebase.database() #conexión a la base de datos
@@ -29,7 +30,7 @@ class index: #pagina raíz
 class login: #pagina login
     def GET(self):
         message = None
-        return render.login(message) #muestra la pagina login.html
+        return render_plain.login(message) #muestra la pagina login.html
 
     def POST(self):
         try:
@@ -45,18 +46,18 @@ class login: #pagina login
             localId = user['localId'] #obtiene el localId del usuario
             #genera una cookie
             web.setcookie('localid', localId)
-            return web.seeother('bienvenida') #muestra la pagina de bienvenida.html, pero no redirecciona
+            return web.seeother('/bienvenida') #muestra la pagina de bienvenida.html, pero no redirecciona
         except Exception as error: #recopila y muestra los datos de algun error
             formato = json.loads(error.args[1])
             error = formato['error']
             message = error['message']
-            print("Error Login.POST: {}".format(message)) #imprime en la terminal el error
-            return render.login(message) #muestra la pagina login.html con el error
+            print("Error login.POST: {}".format(message)) #imprime en la terminal el error
+            return render_plain.login(message) #muestra la pagina login.html con el error
 
 class signup: #pagin sign up
     def GET(self):
         message = None
-        return render.registrar(message)
+        return render_plain.registrar(message)
 
     def POST(self):
         try:
@@ -70,13 +71,13 @@ class signup: #pagin sign up
             user = auth.create_user_with_email_and_password(email, password) #crea un nuevo usuario en firebase
             datos = {'nombre': nombre , 'telefono': telefono , 'email':email} #crea un diccionario con los datos del usuario
             resultados = db.child("usuarios").child(user['localId']).set(datos) #guarda los datos en la base de datos
-            return web.seeother('login') #redirecciona a la pagina del login
+            return web.seeother('/login') #redirecciona a la pagina del login
         except Exception as error: #recopila los datos del error
             formato = json.loads(error.args[1])
             error = formato['error']
             message = error['message']
-            print("Error Login.POST: {}".format(message)) #imprime en la terminal el error
-            return render.registrar(message) #muestra la pagina signup.html con el error
+            print("Error signup.POST: {}".format(message)) #imprime en la terminal el error
+            return render_plain.registrar(message) #muestra la pagina signup.html con el error
 
 class bienvenida: 
     def GET(self):
@@ -89,16 +90,16 @@ class bienvenida:
                 nombre = usuario['nombre'] #obtiene el nombre del usuario
                 return render.bienvenida(nombre) #muestra la pagina bienvenida.html con el nombre del usuario
             else: #si no son iguales, redirecciona a la pagina de login
-                return web.seeother('login') #redireccion la pagina login.html
+                return web.seeother('/login') #redireccion la pagina login.html
 class logout:
     def GET(self):
         web.setcookie('localid', "None") #establece la cookie en None
-        return web.seeother('login') #regresa a la pagina de login
+        return web.seeother('/login') #regresa a la pagina de login
 
 class recuperar:
     def GET(self):
         message = None
-        return render.recuperar(message)
+        return render_plain.recuperar(message)
     def POST(self):
         try:
             #obtiene los datos del formulario
@@ -106,13 +107,13 @@ class recuperar:
             formulario = web.input()
             email = formulario.email
             auth.send_password_reset_email(email) #envia un correo de recuperacion de contraseña
-            return web.seeother('login') #redirecciona a la pagina de login
+            return web.seeother('/login') #redirecciona a la pagina de login
         except Exception as error:
             formato = json.loads(error.args[1])
             error = formato['error']
             message = error['message']
-            print("Error Login.POST: {}".format(message))
-            return render.recuperar(message)
+            print("Error recuperar.POST: {}".format(message))
+            return render_plain.recuperar(message)
 
 class usuarios:
     def GET(self):
@@ -120,7 +121,7 @@ class usuarios:
             users = db.child("usuarios").get() #obtiene todos los usuarios de la base de datos
             return render.usuarios(users) #muestra la pagina usuarios.html
         except Exception as error:
-            print("Error Login.GET: {}".format(error))
+            print("Error usuarios.GET: {}".format(error))
             return render.usuarios(error)
 
 class actualizar_usuario:
@@ -131,18 +132,16 @@ class actualizar_usuario:
         try:
             #obtiene los datos del formulario
             formulario = web.input()
+            id = formulario.id
             email = formulario.email
             nombre = formulario.nombre
             telefono = formulario.telefono
             datos = {'nombre': nombre , 'telefono': telefono , 'email':email} #crea un diccionario con los datos del usuario
-            resultados = db.child("usuarios").child(localId).update(datos) #guarda los datos en la base de datos
-            return web.seeother('usuarios') #redirecciona a la pagina de login
+            resultados = db.child("usuarios").child(id).update(datos) #guarda los datos en la base de datos
+            return web.seeother('/usuarios') #redirecciona a la pagina de usuarios
         except Exception as error:
-            formato = json.loads(error.args[1])
-            error = formato['error']
-            message = error['message']
-            print("Error Login.POST: {}".format(message))
-            return render.actualizar_usuario()
+            print("Error actualizar_usuario.POST: {}".format(error)) #imprime en la terminal el error
+            return web.seeother('/usuarios') #redirecciona a la pagina de usuarios
 
 if __name__ == "__main__": 
     web.config.debug = False
