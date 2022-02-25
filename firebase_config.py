4c0d4156bd7354e3700fa38553cbef617990d5ff
Luis Garcia <77448672+luisgar289@users.noreply.github.com>
2022-02-25 12:52:17 -0600

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,9 +1,9 @@
 firebaseConfig = {
-  "apiKey": "AIzaSyA1sGa2OZ-CBlHAIbP5m0KDPdzH9Rjzb0w",
-  "authDomain": "awi4-4bc78.firebaseapp.com",
-  "databaseURL": "https://awi4-4bc78-default-rtdb.firebaseio.com",
-  "projectId": "awi4-4bc78",
-  "storageBucket": "awi4-4bc78.appspot.com",
-  "messagingSenderId": "974915208569",
-  "appId": "1:974915208569:web:416693cbc76ae595025ba8"
+  "apiKey": "KEY",
+  "authDomain": "project.firebaseapp.com",
+  "databaseURL": "https://project-default-rtdb.firebaseio.com",
+  "projectId": "projectId",
+  "storageBucket": "bucket.appspot.com",
+  "messagingSenderId": "messageID",
+  "appId": "1:000000000000:web:000000abc76ae595025ba8"
 }
